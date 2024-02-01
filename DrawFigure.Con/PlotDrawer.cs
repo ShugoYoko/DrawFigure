3376865859ad83b6c423aa39d304718f61072928
ShugoYoko <ShugoYoko@users.noreply.github.com>
2024-02-01 19:27:19 +0900

    プロジェクト ファイルを追加します。

---
+++ DrawFigure.Con/PlotDrawer.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawFigure.Con
+{
+    internal class PlotDrawer
+    {
+        public PlotDrawer()
+        {
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+
+            ScottPlot.Plot myPlot = new();
+            myPlot.Add.Scatter(dataX, dataY);
+            myPlot.SavePng(@".\quickstart.png", 400, 300);
+        }
+
+    }
+}
